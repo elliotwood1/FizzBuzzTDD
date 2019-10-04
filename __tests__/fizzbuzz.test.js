cc264a1ac2628d8852ef542ea8d5abd83b787019
elliot wood <elliot.wood@ao.com>
2019-10-04 10:54:43 +0100

    Test refactoring

--- __tests__/fizzbuzz.test.js
+++ __tests__/fizzbuzz.test.js
@@ -1,16 +1,16 @@
 const fizzBuzzChecker = require("../fizzbuzz");
 
-describe("If a number is divisible by three replace it with the word 'fizz', if it's divisible by five replace the word with 'buzz' or if it's divisible by three and five then return 'fizz buzz'", function() {
+describe("fizzBuzzChecker", function() {
   it("should return the number if it is not divisble by three or five", function() {
     expect(fizzBuzzChecker(1)).toEqual(1);
   });
-  it("should check if the argument is divisble by three and return 'fizz' if true", function() {
+  it("should return 'fizz' if the argument is divisible by three", function() {
     expect(fizzBuzzChecker(3)).toEqual("fizz");
   });
-  it("should check if the argument is divisble by five and return 'buzz' if true", function() {
+  it("should return 'buzz' if the argument is divisible by five", function() {
     expect(fizzBuzzChecker(5)).toEqual("buzz");
   });
-  it("should check if the argument is divisble by three and five and return 'fizz buzz' if true", function() {
+  it("should return 'fizz buzz' if the argument is divisible by three and five", function() {
     expect(fizzBuzzChecker(15)).toEqual("fizz buzz");
   });
 });
